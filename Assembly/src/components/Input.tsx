9087ab7e555583f3984c8fe6f86d0ac4a13f894e
dot <rawatkushagra33@gmail.com>
2026-01-24 20:52:20 +0530

    re: addLetter and assignColor

--- Assembly/src/components/Input.tsx
+++ Assembly/src/components/Input.tsx
@@ -1,75 +1,110 @@
+import { clsx } from 'clsx';
 import { Keyboard } from './Keyboard';
-import type { Status } from '../App';
+import { buttonPhase, type gameProgress } from '../App';
 
 export function Input({
+  buttonStatus,
+  currentOccurrence,
   currentWord,
+  gameStatus,
   guessedLetters,
-  isCorrect,
+  setButtonStatus,
+  setCurrentOccurrences,
   setGuessedLetters,
-  setIsCorrect,
   setWrongGuesses,
   wrongGuesses,
 }: {
+  buttonStatus: buttonPhase[];
+  currentOccurrence: number[];
   currentWord: string[];
+  gameStatus: gameProgress;
   guessedLetters: string[];
   wrongGuesses: number;
-  isCorrect: Status[];
+  setButtonStatus: (updater: (prev: buttonPhase[]) => buttonPhase[]) => void;
   setGuessedLetters: (updater: (prev: string[]) => string[]) => void;
-  setIsCorrect: (updater: (prev: Status[]) => Status[]) => void;
+  setCurrentOccurrences: (updater: (prev: number[]) => number[]) => void;
   setWrongGuesses: (updater: (prev: number) => number) => void;
 }) {
-  function assignColor(idx: number, status: boolean): void {
+  function assignColor(idx: number, status: buttonPhase) {
     const isFull = guessedLetters.every((element) => element !== '');
     if (!isFull) {
-      setIsCorrect((prev) => {
-        const updatedArray: Status[] = [...prev];
-        updatedArray[idx] = status ? 'correct' : 'incorrect';
+      setButtonStatus((prev) => {
+        const updatedArray: buttonPhase[] = [...prev];
+        updatedArray[idx] = status;
         return updatedArray;
       });
-    } else return;
+    }
   }
 
   function checkLetter(alphabet: string, idx: number): void {
-    if (currentWord.includes(alphabet)) {
-      addLetter(alphabet);
-      assignColor(idx, true);
-    } else {
+    if (gameStatus === 'won' || gameStatus === 'lost') return;
+    const totalOccurrencesOfAlphabet: number = currentWord.filter(
+      (elem) => elem === alphabet,
+    ).length;
+
+    if (totalOccurrencesOfAlphabet === 0) {
       if (wrongGuesses < 8) {
-        assignColor(idx, false);
+        assignColor(idx, buttonPhase.incorrect);
         setWrongGuesses((prev) => prev + 1);
+        return;
+      } else return;
+    }
+
+    if (totalOccurrencesOfAlphabet > 0) {
+      if (currentOccurrence[idx] < totalOccurrencesOfAlphabet - 1) {
+        addLetter(alphabet);
+        assignColor(idx, buttonPhase.highlighted);
+        setCurrentOccurrences((prev) => {
+          const updatedArray: number[] = [...prev];
+          updatedArray[idx] = currentOccurrence[idx] + 1;
+          return updatedArray;
+        });
+      } else if (currentOccurrence[idx] === totalOccurrencesOfAlphabet - 1) {
+        addLetter(alphabet);
+        assignColor(idx, buttonPhase.correct);
+        return;
       }
     }
   }
 
   function addLetter(alphabet: string): void {
     let indexOfAlphabet: number = currentWord.indexOf(alphabet, 0);
-    const empty: string = '';
-    while (
-      indexOfAlphabet !== -1 &&
-      guessedLetters[indexOfAlphabet] !== empty
-    ) {
+    while (indexOfAlphabet !== -1 && guessedLetters[indexOfAlphabet] !== '') {
       indexOfAlphabet = currentWord.indexOf(alphabet, indexOfAlphabet + 1);
     }
     if (indexOfAlphabet === -1) return;
-
-    setGuessedLetters((prev) => {
-      const updatedArray: string[] = [...prev];
-      updatedArray[indexOfAlphabet] = alphabet;
-      return updatedArray;
-    });
+    else
+      setGuessedLetters((prev) => {
+        const updatedArray: string[] = [...prev];
+        updatedArray[indexOfAlphabet] = alphabet;
+        return updatedArray;
+      });
   }
 
-  const mappedArr = guessedLetters.map((letter, idx) => (
+  const currentArray = guessedLetters.map((letter, idx) => (
     <span key={idx} className="span-input">
       {letter}
     </span>
   ));
 
+  const actualArray = guessedLetters.map((letter, idx) => {
+    return (
+      <span
+        key={idx}
+        className={clsx('span-input', letter === '' && 'unguessed')}
+      >
+        {letter === '' ? currentWord[idx] : letter}
+      </span>
+    );
+  });
+
   return (
     <>
-      <div className="input-div">{mappedArr}</div>
+      <div className="input-div">
+        {gameStatus === 'lost' ? actualArray : currentArray}
+      </div>
       <div className="main-keyboard-div">
-        <Keyboard checkLetter={checkLetter} isCorrect={isCorrect} />
+        <Keyboard checkLetter={checkLetter} buttonStatus={buttonStatus} />
       </div>
     </>
   );
