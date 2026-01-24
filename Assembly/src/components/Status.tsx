e350152e69f44f06cc77b38b4e896bb9ec5c8982
dot <rawatkushagra33@gmail.com>
2026-01-24 20:54:41 +0530

    minor fixes

--- Assembly/src/components/Status.tsx
+++ Assembly/src/components/Status.tsx
@@ -1,15 +1,12 @@
 import type { gameProgress } from '../App';
 
-export function Status({
-  gameStatus,
-}: {
-  gameStatus: gameProgress;
-}) {
+export function Status({ gameStatus }: { gameStatus: gameProgress }) {
   type details = {
     status: gameProgress;
     primaryText: string;
     secondaryText: string;
     backgroundColor: string;
+    display: 'flex' | 'none';
   };
 
   const gamePhases: details[] = [
@@ -18,31 +15,37 @@ export function Status({
       primaryText: 'You Win!',
       secondaryText: 'Well Done ! 🎉',
       backgroundColor: 'seagreen',
+      display: 'flex',
     },
     {
       status: 'lost',
       primaryText: 'Game Over!',
       secondaryText: 'You Lose! Better Start Learning Assembly 😭',
       backgroundColor: 'firebrick',
+      display: 'flex',
     },
     {
       status: 'ongoing',
       primaryText: '',
       secondaryText: '',
       backgroundColor: 'inherit',
+      display: 'none',
     },
   ];
 
-    let resultObj!: details;
-    for(const element of gamePhases) {
-	if(element.status === gameStatus){
-	    resultObj = element;
-	}
-}
+  let resultObj!: details;
+  for (const element of gamePhases) {
+    if (element.status === gameStatus) {
+      resultObj = element;
+    }
+  }
 
   return (
-    <div className="status-div">
-      <div className="inner-status-div" style={{ backgroundColor: resultObj.backgroundColor }}>
+    <div className="status-div" style={{ display: resultObj.display }}>
+      <div
+        className="inner-status-div"
+        style={{ backgroundColor: resultObj.backgroundColor }}
+      >
         <p className="status-primary-text">{resultObj.primaryText}</p>
         <p className="status-secondary-text">{resultObj.secondaryText}</p>
       </div>
