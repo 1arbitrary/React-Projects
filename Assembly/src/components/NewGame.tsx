a30786a74341e015f8ad90bd8ae947107d15f79e
dot <rawatkushagra33@gmail.com>
2026-01-24 20:53:11 +0530

    added a reset for currentOccurrences

--- Assembly/src/components/NewGame.tsx
+++ Assembly/src/components/NewGame.tsx
@@ -1,31 +1,35 @@
-import type { gameProgress, Status } from '../App.tsx';
+import { buttonPhase, type gameProgress } from '../App.tsx';
 
 export function NewGame({
   newWord,
+  setButtonStatus,
+  setCurrentOccurrences,
   setCurrentWord,
   setGameStatus,
   setGuessedLetters,
-  setIsCorrect,
   setWrongGuesses,
 }: {
   newWord: () => string[];
+  setButtonStatus: (updater: (prev: buttonPhase[]) => buttonPhase[]) => void;
+  setCurrentOccurrences: (updater: (prev: number[]) => number[]) => void;
   setCurrentWord: (updater: (prev: string[]) => string[]) => void;
-  setGameStatus: (updater: () => gameProgress) => void;
+  setGameStatus: (updater: (prev: gameProgress) => gameProgress) => void;
   setGuessedLetters: (updater: (prev: string[]) => string[]) => void;
-  setIsCorrect: (updater: (prev: Status[]) => Status[]) => void;
-  setWrongGuesses: (updater: () => number) => void;
+  setWrongGuesses: (updater: (prev: number) => number) => void;
 }) {
-  function generateWord(): void {
+  function resetGameLogic(): void {
     const updatedWord: string[] = newWord();
+    const keyboardSize: number = 26;
     setCurrentWord(() => updatedWord);
     setGuessedLetters(() => new Array(updatedWord.length).fill(''));
-    setIsCorrect((prev) => prev.map(() => 'undecided'));
+    setButtonStatus(() => new Array(keyboardSize).fill(buttonPhase.idle));
     setWrongGuesses(() => 0);
+    setCurrentOccurrences(() => new Array(keyboardSize).fill(1));
     setGameStatus(() => 'ongoing');
   }
   return (
     <div className="new-game-div">
-      <button className="new-game-btn" onClick={generateWord}>
+      <button className="new-game-btn" onClick={resetGameLogic}>
         New Game
       </button>
     </div>
